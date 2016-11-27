a64a22d424a653e3955cdb003875c91e3f6fa772
Riza Fahmi <rizafahmi@gmail.com>
2016-11-27 21:29:59 +0700

    Finish our first task in todo: connect to API

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 // Problem: We need a simple way to look at JavaScript Developer in Indonesia
 // Solution: Use NodeJS to connect to GitHub API to get profile information and print out to the console
 
-// TODO: Connect to the API with specific queries (https://api.github.com/users/username)
-// TODO: Read the data
-// TODO: Parse the data
-// TODO: Print the data out
+'use strict'
+const https = require('https')
+
+const username = 'rizafahmi'
+// Connect to the API with specific queries (https://api.github.com/users/username)
+const options = {
+  hostname: `api.github.com`,
+  port: 443,
+  path: `/users/${username}`,
+  method: 'GET',
+  headers: {
+    'user-agent': 'node.js'
+  }
+}
+let request = https.request(options, (response) => {
+    console.log(response.statusCode)
+    // TODO: Read the data
+    // TODO: Parse the data
+    // TODO: Print the data out
+
+})
+
+request.end()
+
+request.on('error', (e) => {
+  console.error(e)
+})
