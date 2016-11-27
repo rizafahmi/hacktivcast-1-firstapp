add0ea8630d20092184a10264bc098d9ca08b654
Riza Fahmi <rizafahmi@gmail.com>
2016-11-27 22:44:08 +0700

    Prints out the data using our own function creation

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
 'use strict'
 const https = require('https')
 
+let printData = (name, public_repos, followers) => {
+  return `${name} owns ${public_repos} repo(s) and has ${followers} follower(s)`
+}
+
 const username = 'rizafahmi'
 // [x] Connect to the API with specific queries (https://api.github.com/users/username)
 const options = {
@@ -26,8 +30,9 @@ let request = https.request(options, (response) => {
   response.on('end', () => {
     // [x] Parse the data
     let profile = JSON.parse(body)
-    console.log(profile)
-    // TODO: Print the data out
+    // [x] Print the data out
+    // prints out name, public_repos, followers
+    console.log(printData(profile.name, profile.public_repos, profile.followers))
   })
 
 })
