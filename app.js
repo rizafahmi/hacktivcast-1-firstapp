2b5ede043e8054581b74594f70e39bf150fe2e68
Riza Fahmi <rizafahmi@gmail.com>
2016-11-27 20:59:13 +0700

    Update the instruction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Problem: We need a simple way to look at JavaScript Developer in Indonesia
 // Solution: Use NodeJS to connect to GitHub API to get profile information and print out to the console
 
-// TODO: Connect to the API with specific queries
+// TODO: Connect to the API with specific queries (https://api.github.com/users/username)
 // TODO: Read the data
 // TODO: Parse the data
 // TODO: Print the data out
