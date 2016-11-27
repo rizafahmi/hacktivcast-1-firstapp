a465969f266ada426f067a1dcd2ae1e36a2a8441
Riza Fahmi <rizafahmi@gmail.com>
2016-11-27 20:52:22 +0700

    Add app.js with instruction (problem and solution)

---
+++ app.js
@@ -0,0 +1,6 @@
+// Problem: We need a simple way to look at JavaScript Developer in Indonesia
+// Solution: Use NodeJS to connect to GitHub API to get profile information and print out to the console
+// Connect to the API with specific queries
+// Read the data
+// Parse the data
+// Print the data out
