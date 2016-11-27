1791a87638c75d6b2ab6904a18a5c52b768ee005
Riza Fahmi <rizafahmi@gmail.com>
2016-11-27 20:54:25 +0700

    Add todo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Problem: We need a simple way to look at JavaScript Developer in Indonesia
 // Solution: Use NodeJS to connect to GitHub API to get profile information and print out to the console
-// Connect to the API with specific queries
-// Read the data
-// Parse the data
-// Print the data out
+
+// TODO: Connect to the API with specific queries
+// TODO: Read the data
+// TODO: Parse the data
+// TODO: Print the data out
